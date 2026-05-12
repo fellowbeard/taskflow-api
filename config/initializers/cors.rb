1262e435ab690a12fad51e5cb23c00a6ef25e720
fellowbeard <virgovibe@gmail.com>
2026-05-12 07:55:35 -0500

    Allow deployed frontend origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("FRONTEND_URL", "http://localhost:5173")
+    origins "http://localhost:5173", "https://taskflow-frontend-eta-pink.vercel.app"
 
     resource "*",
       headers: :any,
